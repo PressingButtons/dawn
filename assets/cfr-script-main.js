126840c2708f3a755030bb4c3959d71e31b48701
Alex Ilea <6627543+alisterdev@users.noreply.github.com>
2023-12-21 12:02:43 -0500

    Update to add multiple theme blocks

---
+++ assets/cfr-script-main.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', async event => {
+    /*
+    const stickyHeader = document.querySelector('sticky-header');
+    const stickyHeader_height = stickyHeader.getBoundingClientRect( ).height;
+
+    if( document.documentElement.scrollTop > stickyHeader.getBoundingClientRect( ).height ) {
+        stickyHeader.classList.remove('cfr-sticky_header');
+    }
+
+    window.addEventListener('scroll', event => {
+        // dynamic sticky header 
+        if( document.documentElement.scrollTop > stickyHeader_height ) {
+            stickyHeader.classList.remove('cfr-sticky_header');
+        } else {
+            stickyHeader.classList.add('cfr-sticky_header');
+        }
+
+    });
+    */
+});
+
